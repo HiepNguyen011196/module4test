c9ccbab77b59478f85a229e80147c63b6c26d352
Hiep Nguyen <gagga5376@gmail.com>
2022-10-12 10:58:52 +0700

    xong

---
+++ src/main/java/com/example/finalexample4country/PracticeTestModule4Application.java
@@ -0,0 +1,13 @@
+package com.example.finalexample4country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTestModule4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeTestModule4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalexample4country/PracticeTestModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalexample4country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTestModule4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
